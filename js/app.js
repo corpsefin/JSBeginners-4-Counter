ce518439f776113a53033052130def616bd90b4b
Ville Lyytikäinen <corpsefin@gmail.com>
2020-01-27 09:30:42 +0200

    completed project

---
+++ js/app.js
@@ -0,0 +1,27 @@
+(function(){
+    let counter = document.getElementById('counter');
+    let container = document.getElementById('container');
+    let decreaseButton = document.getElementById('decrease');
+    let increaseButton = document.getElementById('increase');
+    let buttonContainer = document.getElementById('buttons');
+    let counterValue = 0;
+
+    counter.textContent = counterValue;
+
+    buttonContainer.addEventListener('click', (e) =>{
+        e.preventDefault();
+        changeValue(e.target);
+    })
+
+    function changeValue(target){
+        if(target === decreaseButton){
+            counterValue--;
+            counter.textContent = counterValue;
+        }
+        else if(target === increaseButton){
+            counterValue++;
+            counter.textContent = counterValue;
+        }
+    }
+
+})();
